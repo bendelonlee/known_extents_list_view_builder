6efb7b8ca403ebc6acfc6bbbc5b03df08c16b19e
Benjamin Lee <bendelonlee@gmail.com>
2021-07-07 09:52:05 -0600

    WIP

---
+++ lib/known_extents_list_view_builder.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:known_extents_list_view_builder/sliver_known_extents_list.dart';
+
+class KnownExtentsListView extends BoxScrollView {
+  final List<double> itemExtents;
+  final SliverChildBuilderDelegate childrenDelegate;
+  KnownExtentsListView.builder({
+    required this.itemExtents,
+    Key? key,
+    Axis scrollDirection = Axis.vertical,
+    bool reverse = false,
+    ScrollController? controller,
+    bool? primary,
+    ScrollPhysics? physics,
+    bool shrinkWrap = false,
+    EdgeInsetsGeometry? padding,
+    required IndexedWidgetBuilder itemBuilder,
+    int? itemCount,
+    bool addAutomaticKeepAlives = true,
+    bool addRepaintBoundaries = true,
+    bool addSemanticIndexes = true,
+    double? cacheExtent,
+    int? semanticChildCount,
+    DragStartBehavior dragStartBehavior = DragStartBehavior.start,
+    ScrollViewKeyboardDismissBehavior keyboardDismissBehavior =
+        ScrollViewKeyboardDismissBehavior.manual,
+    String? restorationId,
+    Clip clipBehavior = Clip.hardEdge,
+  })  : assert(itemCount == null || itemCount >= 0),
+        assert(semanticChildCount == null || semanticChildCount <= itemCount!),
+        childrenDelegate = SliverChildBuilderDelegate(
+          itemBuilder,
+          childCount: itemCount,
+          addAutomaticKeepAlives: addAutomaticKeepAlives,
+          addRepaintBoundaries: addRepaintBoundaries,
+          addSemanticIndexes: addSemanticIndexes,
+        ),
+        super(
+          key: key,
+          scrollDirection: scrollDirection,
+          reverse: reverse,
+          controller: controller,
+          primary: primary,
+          physics: physics,
+          shrinkWrap: shrinkWrap,
+          padding: padding,
+          cacheExtent: cacheExtent,
+          semanticChildCount: semanticChildCount ?? itemCount,
+          dragStartBehavior: dragStartBehavior,
+          keyboardDismissBehavior: keyboardDismissBehavior,
+          restorationId: restorationId,
+          clipBehavior: clipBehavior,
+        );
+  @override
+  Widget buildChildLayout(BuildContext context) {
+    return SliverKnownExtentsList(
+      delegate: childrenDelegate,
+      itemExtents: itemExtents,
+    );
+  }
+}
+
---
+++ lib/sliver_known_extents_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:known_extents_list_view_builder/render_sliver_known_extents_list.dart';
+
+class SliverKnownExtentsList extends SliverMultiBoxAdaptorWidget {
+  /// Creates a sliver that places box children with the same main axis extent
+  /// in a linear array.
+  const SliverKnownExtentsList({
+    Key? key,
+    required SliverChildDelegate delegate,
+    required this.itemExtents,
+  }) : super(key: key, delegate: delegate);
+
+  /// The extent the children are forced to have in the main axis.
+  final List<double> itemExtents;
+
+  @override
+  RenderSliverKnownExtentsList createRenderObject(BuildContext context) {
+    final SliverMultiBoxAdaptorElement element = context as SliverMultiBoxAdaptorElement;
+    return RenderSliverKnownExtentsList(childManager: element, itemExtents: itemExtents);
+  }
+
+  @override
+  void updateRenderObject(BuildContext context, RenderSliverFixedExtentList renderObject) {
+    
+    renderObject.itemExtent = itemExtents[0];
+  }
+}
